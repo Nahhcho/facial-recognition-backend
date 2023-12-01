76e6dbc7bfea2776822f3d3b4ae89ff9f1e05761
Jimmy Bonilla <jimmyb091202@gmail.com>
2023-12-01 13:23:17 -0800

    added connection string

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const db = knex({
     client: 'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString : process.env.DATABASE_URL,
         ssl: true,
     }
 })
